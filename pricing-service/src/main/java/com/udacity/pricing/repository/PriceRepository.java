ab7ddd4d5df91622a13adefeef8d5a907505444d
refka-farid <refka.farid@gmail.com>
2021-07-21 18:30:21 +0200

    [vehiclesApi] Add Test to Pricing Service Application .

--- pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -1,7 +1,6 @@
 package com.udacity.pricing.repository;
 
 import com.udacity.pricing.entity.Price;
-//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,60 @@
 package com.udacity.pricing;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class PricingServiceApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.udacity.pricing.entity.Price;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//}
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingServiceApplicationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @JsonProperty("collection")
+    private Collection<Price> collection;
+
+	@Test
+	public void contextLoads() {
+	}
+
+    @Test
+    public void getAllPrices()  {
+        var response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/prices/", Array.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void gePrice() {
+        ResponseEntity<Price> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/prices/1", Price.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+}
+
